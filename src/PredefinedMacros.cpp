ce578542312f9ebf9a1ce2d3257c79202395c1f9
nkh-lab <nkh@ua.fm>
2017-05-14 23:12:32 +0300

    PredefinedMacros: test with __PRETTY_FUNCTION__ added.

--- src/PredefinedMacros.cpp
+++ src/PredefinedMacros.cpp
@@ -7,6 +7,20 @@ namespace PredefinedMacros {
 void test(void)
 {
     std::cout << __FUNCTION__ << "() in file " << __FILE__ << " on line " << __LINE__ << "\n";
+
+    /* output:
+
+    test() in file /home/nkh/my/cpp/src/PredefinedMacros.cpp on line 9
+
+    */
+
+    std::cout << __PRETTY_FUNCTION__ << std::endl;
+
+    /* output:
+
+    void PredefinedMacros::test()
+
+    */
 }
 
 }
