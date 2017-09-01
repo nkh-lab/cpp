33c046c16859abb921d1de50f355288396d64300
Nikolay Khilyuk <nkhilyuk@luxoft.com>
2017-09-01 11:31:54 +0300

    Extra test with smart pointer.

--- src/SmartPointer.cpp
+++ src/SmartPointer.cpp
@@ -6,21 +6,38 @@ namespace SmartPointer {
 
 void test(void)
 {
-    clsA a((char*)"A");
-    clsA* pA = new clsA((char*)"pA");
+    // constructor with managed object
+    {
+        clsA a((char*)"A");
+        clsA* pA = new clsA((char*)"pA");
 
-    if(pA != nullptr);// to suppress warning: unused variable ‘pA’ [-Wunused-variable]
+        if(pA != nullptr);// to suppress warning: unused variable ‘pA’ [-Wunused-variable]
 
-    std::unique_ptr<clsA> upA(new clsA((char*)"upA"));
+
+        std::unique_ptr<clsA> upA(new clsA((char*)"upA"));
+
+        /* output:
+        clsA constructor for Name = A
+        clsA constructor for Name = pA
+        clsA constructor for Name = upA
+        clsA destructor  for Name = upA
+        clsA destructor  for Name = A
+        */
+        // ! Object pointed by play pointer 'pA' was not destroyed
+    }
+
+
+    // constructor with no managed object
+    std::unique_ptr<clsA> upANoM;
+
+    if(upANoM == nullptr) upANoM->testInline();
 
     /* output:
-    clsA constructor for Name = A
-    clsA constructor for Name = pA
-    clsA constructor for Name = upA
-    clsA destructor  for Name = upA
-    clsA destructor  for Name = A
+
+    clsA testInline()
+
     */
-    // ! plain pointer 'pA' didn't destroy its object but smart 'upA' did
+    // ! No constructor and destructor call but method could be called
 }
 
 }
