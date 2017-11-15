e77df3dbb39c68b7a079e141d49bf98492a3832a
Nikolay Khilyuk <nkhilyuk@luxoft.com>
2017-11-15 13:19:47 +0200

    Add one shot event logic and description in doxygen style.

--- src/Events.hpp
+++ src/Events.hpp
@@ -3,17 +3,25 @@
 
 #include <functional>
 #include <map>
+#include <list>
 #include <algorithm>
 #include <mutex>
 
 namespace Events {
 
+/**
+ * @brief The VoidClass class is used in UnsafeSyncPolicy like stub for Locker_t.
+ */
 class VoidClass
 {
 
 };
 
-class UnsafeSyncPolicy // without synchronization mechanism, e.g. event provider and consumer are in the single thread
+/**
+ * @brief The UnsafeSyncPolicy class is used to determine strategy for Event class.
+ * It doesn't include synchronization mechanism and can be used when event provider and consumer are in the single thread.
+ */
+class UnsafeSyncPolicy
 {
 public:
     typedef VoidClass Locker_t;
@@ -21,7 +29,11 @@ public:
     UnsafeSyncPolicy(Locker_t& locker) {}
 };
 
-class SafeSyncPolicy // with synchronization mechanism
+/**
+ * @brief The SafeSyncPolicy class is used to determine strategy for Event class.
+ * Includs synchronization mechanism based on Locker_t type.
+ */
+class SafeSyncPolicy
 {
 public:
     typedef std::mutex Locker_t;
@@ -41,17 +53,34 @@ private:
 };
 
 template <typename TSyncPolicy, typename... TEvtArgs>
+/**
+ * @brief Event template class based on templates:
+ * TSyncPolicy - describes synchronisation starategy.
+ * TEvtArgs    - describes variadic arguments for event.
+ *
+ * @warning subscribe() and unsubscribe() mast not invoke from evnt handler on consumer side.
+ * It leads to deadlock (if SafeSyncPolicy is used).
+ * To unsubsribe from event handler 'oneShot = true' in subscribe() can be used.
+ */
 class Event
 {
     typedef std::function<void(const TEvtArgs&...)> Handler_t;
     typedef std::map<int /*handlerID*/, Handler_t>  Handlers_t;
 
     Handlers_t mHandlers;
+    std::list<int> mOneShotHandlerIDs;
 
     typename TSyncPolicy::Locker_t mLocker;
 
 public:
-    int subscribe(const Handler_t& handler)
+    /**
+     * @brief Subscribes to event.
+     * @param handler - binded event handler.
+     * @param oneShot - true  - handler automatically is usubscribed after fist event rising,
+     *                  false (default) - envents are rised until unsubscribe() invoking.
+     * @return Handler ID.
+     */
+    int subscribe(const Handler_t& handler, bool oneShot = false)
     {
         TSyncPolicy guard(mLocker);
 
@@ -59,9 +88,14 @@ public:
 
         mHandlers.insert( std::pair<int /*handlerID*/, Handler_t>(handlerID, handler));
 
+        if (oneShot) mOneShotHandlerIDs.push_back(handlerID);
+
         return handlerID;
     }
-
+    /**
+     * @brief Unsubscribes from event.
+     * @param handlerID - ID of the handler which should be unsubscribed.
+     */
     void unsubscribe(const int& handlerID)
     {
         TSyncPolicy guard(mLocker);
@@ -69,6 +103,10 @@ public:
         mHandlers.erase(handlerID);
     }
 
+    /**
+     * @brief Rises the event CBs.
+     * @param val - event variadic arguments values depends on event type.
+     */
     void rise(const TEvtArgs&... val)
     {
         TSyncPolicy guard(mLocker);
@@ -77,6 +115,14 @@ public:
         {
             it->second(val...);
         }
+
+        // remove One Shot events
+        for(auto it = mOneShotHandlerIDs.begin(); it != mOneShotHandlerIDs.end(); ++it)
+        {
+           mHandlers.erase(*it);
+        }
+
+        mOneShotHandlerIDs.clear();
     }
 
 private:
@@ -93,12 +139,6 @@ private:
         return ret;
     }
 };
-
-
-
-
-
-
 
 }
 
